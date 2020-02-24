8dd54a45af12fc81568f527f13c92818c86c78b6
ikawashima41 <i.kawashima41@gmail.com>
2020-02-24 18:32:01 +0900

    Add Unit Test

---
+++ BookManagerWithSwiftUI/View/Home/ViewData/BookRowData.swift
@@ -0,0 +1,15 @@
+//
+//  BookRowData.swift
+//  BookManagerWithSwiftUI
+//
+//  Created by Iichiro Kawashima on 2020/02/24.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+struct BookRowData: Identifiable {
+    var id: Int
+    var title: String
+    var price: String
+    var purchaseDate: String
+    var image: String
+}
--- BookManagerWithSwiftUITests/BookManagerWithSwiftUITests.swift
+++ BookManagerWithSwiftUITests/BookManagerWithSwiftUITests.swift
@@ -7,9 +7,14 @@
 //
 
 import XCTest
+import Combine
+
 @testable import BookManagerWithSwiftUI
 
 class BookManagerWithSwiftUITests: XCTestCase {
+
+    let client = BookAPIClient()
+    var cancellableSet: Set<AnyCancellable> = []
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,15 +25,48 @@ class BookManagerWithSwiftUITests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let exp = expectation(description: "register book")
+
+        client.registerBook(params: BookParams(id: 10, title: "Test", price: 1000, purchaseDate: "2020/01/01", image: ""))?
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    XCTAssertFalse(false)
+                case .finished:
+                    print("Finished")
+                }
+            }, receiveValue: { (result) in
+                print(result)
+                exp.fulfill()
+            })
+            .store(in: &cancellableSet)
+
+            wait(for: [exp], timeout: 20.0)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    func testFetchExample() {
 
+        let exp = expectation(description: "fetch book")
+
+        client.fetchBooks(params: BookListParams(page: 1, limit: 10))?
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    XCTAssertFalse(false)
+                case .finished:
+                    print("Finished")
+                }
+            }, receiveValue: { (result) in
+                print(result)
+                exp.fulfill()
+            })
+            .store(in: &cancellableSet)
+
+            wait(for: [exp], timeout: 20.0)
+    }
 }
