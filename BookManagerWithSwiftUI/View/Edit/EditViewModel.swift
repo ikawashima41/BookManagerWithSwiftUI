79bac95e776a63bb20827b001c3baf8f4ea5ae01
ikawashima41 <i.kawashima41@gmail.com>
2020-02-24 18:39:30 +0900

    Add viewModel to methods

--- BookManagerWithSwiftUI/View/Edit/EditViewModel.swift
+++ BookManagerWithSwiftUI/View/Edit/EditViewModel.swift
@@ -10,5 +10,28 @@ import Foundation
 import Combine
 
 final class EditViewModel: ObservableObject, Identifiable {
+    private let client: AccountAPIClient
+    private var cancellableSet: Set<AnyCancellable> = []
 
+    init(_ client: AccountAPIClient = AccountAPIClient()) {
+        self.client = client
+    }
+
+    func registerBook() {
+        let client = BookAPIClient()
+
+        client.updateBook(params: BookParams(id: 10, title: "", price: 1000, purchaseDate: "", image: ""))?
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                case .finished:
+                    print("Finished")
+                }
+            }, receiveValue: { (result) in
+                print(result)
+            })
+            .store(in: &cancellableSet)
+    }
 }
